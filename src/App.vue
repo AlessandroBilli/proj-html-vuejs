53556a64f30f6e8236730f437887c7f2485e9891
AlessandroBilli <skillmanbungo@gmail.com>
2023-12-19 18:37:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+
+
+
+
+
+};
+</script>
+
+<template>
+  <div>
+
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    // Logica JavaScript per il componente
+};
+</script>
+
+<template>
+    <div>
+        <!-- Contenuto del componente -->
+    </div>
+</template>
+
+<style>
+/* Stili CSS specifici del componente */
+</style>
