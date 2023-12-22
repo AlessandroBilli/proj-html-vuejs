8f9521dcfe6bb06043712787c46a24cdf0ea6402
AlessandroBilli <skillmanbungo@gmail.com>
2023-12-22 16:56:25 +0100

    feat: contact

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -20,6 +20,19 @@ export default {
             <img src="../../public/img/h3-product-img-6a-100x100.png" alt="">
 
         </div>
+        <div class="contacts">
+            <h1>Lista Contatti Pizzeria</h1>
+            <ul>
+                <li>
+                    <span class="contact-name">Pizzeria Don Peppe</span>
+                    <span class="contact-info">Via Roma, 123 - 00100, Città</span>
+                    <span class="contact-info">Telefono: 062-3456789</span>
+                    <span class="contact-info">Email: info@pizzeriadonpeppe.com</span>
+                </li>
+
+
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -28,6 +41,10 @@ export default {
     display: flex;
     justify-content: center;
     margin-bottom: 100px;
+    font-family: Arial, sans-serif;
+    font-weight: bold;
+
+    ;
 }
 
 .pizza-container {
@@ -42,5 +59,53 @@ export default {
 
 img {
     width: 100%;
+}
+
+
+body,
+h1,
+ul,
+li {
+    margin: 0;
+    padding: 0;
+}
+
+
+.contacts {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    font-family: Arial, sans-serif;
+}
+
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+
+ul {
+    list-style-type: none;
+}
+
+
+li {
+    border-bottom: 1px solid #ccc;
+    padding: 10px 0;
+}
+
+
+.contact-name {
+    font-weight: bold;
+    font-size: 18px;
+}
+
+
+.contact-info {
+    display: block;
+    font-size: 14px;
+    color: #555;
+    margin-top: 5px;
 }
 </style>
